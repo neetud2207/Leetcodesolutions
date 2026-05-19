3ac25d199fef1299cad8b5c71c9d6d884abcacbc
Nithyassri Dhanaraj <nithyassri.07@gmail.com>
2026-05-19 13:55:09 +0530

    Create 1287-element-appearing-more-than-25-percent.java

---
+++ 1287-element-appearing-more-than-25-percent.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int n= arr.length;
+        int q=n/4;
+        for(int i=0;i<n-q;i++){
+            if(arr[i]==arr[i+q])
+              return arr[i];
+
+        }
+        return arr[0];
+    }
+}
